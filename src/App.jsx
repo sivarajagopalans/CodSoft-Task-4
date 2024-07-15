2c926f51c138233b0c7aaf3cf9b4ee95d03c83b1
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-15 12:39:13 +0530

    install react bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
 
   return (
     <>
-     
+  
     </>
   )
 }
