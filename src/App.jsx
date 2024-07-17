8c7685e593f33e5bfef4fb42a2089fe749fe2661
sivarajagopalans <sivarajagopalans@gmail.com>
2024-07-17 17:03:34 +0530

    add Categories component

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { NavbarComponent } from './components/NavbarComponent';
+import { Cards } from './components/Cards';
+import { Categories } from './components/Categories';
 
 function App() {
 
   return (
     <>
       <NavbarComponent/>
+      {/* <Cards/> */}
+      <Categories/>
     </>
   )
 }
